7dfbd7798c389a91066833468120158b3c1b40bb
Jonathan <jonathanperalta1893@gmail.com>
2019-01-28 16:36:46 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo "<a href=".route('contactos').">Contactos</a>"; //nombre de la ruta
 });
+
+Route::get('/contactame', ['as' => 'contactos',function () { //nombre de la ruta
+    return "Ginny I love you babe ";
+}])->where('nombre','[A-Za-z]+');
+
+
+Route::get('/saludo/{nombre?}', function ($nombre = 'GINEVRA') { // ? no-require
+    return "Hello babe ".$nombre;
+})->where('nombre','[A-Za-z]+'); // validacion
