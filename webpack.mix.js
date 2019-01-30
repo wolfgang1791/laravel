e094e26a3cefb73cd8e025c09d2ccfbef1b32d06
Jonathan <jonathanperalta1893@gmail.com>
2019-01-29 22:54:04 -0500

    wea bootstrap.bundle.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ mix.js('resources/js/app.js', 'public/js')
    .sass('resources/assets/scss/app.scss', 'public/css')
    .scripts([
        'node_modules/jquery/dist/jquery.js',
-       'node_modules/bootstrap/dist/js/bootstrap.js',
-       'node_modules/popper.js/dist/popper.js',
+       'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+      // 'node_modules/popper.js/dist/popper.js',
    ],'public/js/all.js');
    //.styles(['a.css','b.css','c.css'],'public/css/d.css','public/css');
