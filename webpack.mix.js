f460eaccee76c46d4ea9b69c055de09033ee2050
Jonathan <jonathanperalta1893@gmail.com>
2019-01-29 23:15:11 -0500

    js propios y browser Sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,9 @@ mix.js('resources/js/app.js', 'public/js')
    .scripts([
        'node_modules/jquery/dist/jquery.js',
        'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-      // 'node_modules/popper.js/dist/popper.js',
-   ],'public/js/all.js');
+       'resources/assets/js/app.js'
+   ],'public/js/all.js','./')
+   .browserSync({
+       'proxy': 'http://localhost:8000/'
+   });
    //.styles(['a.css','b.css','c.css'],'public/css/d.css','public/css');
